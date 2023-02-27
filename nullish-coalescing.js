4f309fdfbb2ccffd6036810049efcef0233ecb96
superman <goodnesskolapo@gmail.com>
2023-02-27 19:24:36 +0100

    this

---
+++ nullish-coalescing.js
@@ -0,0 +1,8 @@
+const userName = undefined;
+const userName2 = "nigeria";
+
+const user = `welcome, ${userName ?? 'user'}`
+const user2 = `welcome, ${userName2 ?? 'user2'}`
+
+console.log(user);
+console.log(user2);

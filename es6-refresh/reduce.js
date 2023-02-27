d6b1f2dcc7ab5fb9d8a54a3c8e31d1300597a891
superman <goodnesskolapo@gmail.com>
2023-02-27 21:39:00 +0100

    hmmm

--- es6-refresh/reduce.js
+++ es6-refresh/reduce.js
@@ -1,3 +1,10 @@
 let salaries = [4000, 2500, 800, 15000, 750, 5000]
 
-let x = salaries.reduce()
+let x = salaries.reduce((x, y) => x + y, 0)
+let y = salaries.reduce((x, y) => x * y, 1)
+let z = salaries.reduce((x, y) => x % y, 1)
+
+console.log(x);
+console.log(y);
+console.log(z);
+// console.log(4000 + 2500 + 800 + 15000 + 750 + 5000);

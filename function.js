4bcf9fbf54e05332f8b8ed34352ca61230b9d281
superman <goodnesskolapo@gmail.com>
2023-02-27 19:37:39 +0100

    this

--- function.js
+++ function.js
@@ -12,6 +12,12 @@ const funcname = function () {
 }
 
 // 3. Arrow functions
+// * the THIS keyword does not work in an arrow function
 const functionname = () =>{
     // ...
 }
+
+const sum = (a, b) =>{
+    return a+b;
+}
+console.log(sum(10, 30));

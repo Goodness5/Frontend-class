76f67b7f7da8d5ef84e9995440635afedc144cfc
superman <goodnesskolapo@gmail.com>
2023-02-27 19:27:58 +0100

    this

---
+++ function.js
@@ -0,0 +1,17 @@
+// three ways to declear a function
+
+
+// 1. normal function 
+function nameoffunction() {
+    // .....
+}
+
+// 2. expression function
+const funcname = function () {
+    // ...
+}
+
+// 3. Arrow functions
+const functionname = () =>{
+    // ...
+}

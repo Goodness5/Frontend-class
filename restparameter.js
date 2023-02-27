d14acd0051d73ad07ba6d7232f7c0df6f0c8c736
superman <goodnesskolapo@gmail.com>
2023-02-27 18:15:21 +0100

    small test

--- restparameter.js
+++ restparameter.js
@@ -1,7 +1,11 @@
 function Sum(...x) {
-    let total = x;
+    let total = 0;
 
+    for(const arg of x){
+        total += arg;
+    }
     return total;
 }
 
-console.log(Sum(2));
+console.log(Sum(2, 3, 4, 7));
+// console.log(typeof(Sum(3)));

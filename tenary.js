5e4cfc4b9a14018d2e6b1237eb4a9cd408a05fa3
superman <goodnesskolapo@gmail.com>
2023-02-27 17:24:18 +0100

    small test

--- tenary.js
+++ tenary.js
@@ -1,7 +1,26 @@
-function checkAge(age) {
-  let x = age >= 18 ? "eligible" : "not eligible";
+// function checkAge(age) {
+//   let x = age >= 18 ? "eligible" : "not eligible";
 
-  return x;
+//   return x;
+// }
+
+// console.log(checkAge(18));
+
+// a building has 16 floors, a user is in floor 1 - 4nshould say "web3bridge", a user in floor 5 -8
+// should say "nigeria", a user on floor 9 -12 should say "smart-contract"
+// other users should say "blockchain"
+
+function building (floor) {
+  let statement =
+    floor <= 4
+      ? 'web3bridge'
+      : floor >= 5 && floor < 9
+      ? 'Nigeria'
+      : floor >= 9 && floor < 13
+      ? 'smart contract'
+      : 'blockchain'
+
+      return statement;
 }
 
-console.log(checkAge(18));
+console.log(building(16));

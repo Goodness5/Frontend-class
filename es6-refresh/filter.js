019a3b6bb83296782e4556bcea8c42279ac26c17
superman <goodnesskolapo@gmail.com>
2023-02-28 17:10:43 +0100

    this

--- es6-refresh/filter.js
+++ es6-refresh/filter.js
@@ -1,5 +1,18 @@
-const numbers = [23, 45, 16, 34, 67, 20, 63, 89];
+const numbers = [23, 45, 16, 34, 60, 67, 59, 20, 63, 89];
 
 const x = numbers.filter(number => number >= 60);
-
+// const z = numbers.filter((number, x, y) => console.log(number, x, y));
 console.log(x);
+// console.log(z);
+
+
+const users = [
+{name: "jerry", age: 13, isAdmin: true},
+{name: "chris", age: 13, isAdmin: false},
+{name: "samuel", age: 13, isAdmin: false},
+{name: "david", age: 13, isAdmin: false},
+{name: "musa", age: 13, isAdmin: true}
+
+]
+
+const x = users.map(user, _i) =>
---
+++ typescript/arrays.ts
@@ -0,0 +1,15 @@
+interface Iusers {
+    name: string,
+    age: number,
+    isAdmin: boolean,
+}
+
+
+const user: Iusers[] = [
+    {name: "jerry", age: 13, isAdmin: true},
+    {name: "chris", age: 13, isAdmin: false},
+    {name: "samuel", age: 13, isAdmin: false},
+    {name: "david", age: 13, isAdmin: false},
+    {name: "musa", age: 13, isAdmin: true}
+    
+    ]
